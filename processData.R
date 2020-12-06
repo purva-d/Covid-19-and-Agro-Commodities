bbc291193afd87a74b0100e45257953c78bde377
purva-d <purvadandekar98@gmail.com>
2020-12-06 17:26:31 +0530

    Initial processing of data

--- processData.R
+++ processData.R
@@ -11,15 +11,20 @@ filterData <- filter(readdata,(mp_year > "2018" & pt_name == "Retail"))
 ## Drop un-necessary columns
 drop.cols <- c("adm0_id","adm1_name","adm1_id","mkt_id","cur_id","pt_id","pt_name","cm_id","mp_commoditysource","um_id")
 filterData <- select (filterData,-all_of(drop.cols))
+
+# adm0_name is country name
 filterData <- rename(filterData,c('country_name' = 'adm0_name'))
+
+countries <- unique(filterData$country_name)
+countries
+
 ## Bassas the India is India
 filterData$country_name[which(filterData$country_name == "Bassas da India")] <- "India"
-countries <- unique(filterData$adm0_name)
-countries
+
+
 ## R date object cant be converted into csv files wither drop it or 
 ##use character version of date 01-01-2010
 ## convert month and year as date object
-## google data studio supports format YYYYMMDD
 filterData$Date <- paste("1-",filterData$mp_month,"-",filterData$mp_year,sep = "")
 
 ## Dropping year and month columns
